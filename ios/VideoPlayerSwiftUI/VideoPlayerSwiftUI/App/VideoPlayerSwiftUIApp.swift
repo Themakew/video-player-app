2dd8748c22021dcee123bcbc3d566d43bcd59762
Ruyther Costa <rcosta.datum@digio.com.br>
2024-01-14 15:04:01 -0700

    Structing folders in the project file to match the architecture pattern chose, MVVM.

--- ios/VideoPlayerSwiftUI/VideoPlayerSwiftUI/App/VideoPlayerSwiftUIApp.swift
+++ ios/VideoPlayerSwiftUI/VideoPlayerSwiftUI/App/VideoPlayerSwiftUIApp.swift
@@ -9,6 +9,11 @@ import SwiftUI
 
 @main
 struct VideoPlayerSwiftUIApp: App {
+
+    init() {
+        UIView.appearance().overrideUserInterfaceStyle = .light
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
